007ef01d906023c35b97032cdc2aa27ccb273761
Louis Turpault <louis.turpault@gmail.com>
2026-06-15 14:12:35 -0400

    fix: fixed repro.lua

--- repro.lua
+++ repro.lua
@@ -2,4 +2,6 @@ vim.pack.add({
   'https://github.com/saghen/blink.download',
   { src = 'https://github.com/saghen/blink.pairs', version = vim.version.range('*') },
 })
-require('blink.pairs').setup({})
+local blink = require('blink.pairs')
+blink.build():pwait(60000)
+blink.setup({})
